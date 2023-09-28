15a8fdad6d57c29a29290c51f937117808cdb49b
Aditya Agarwal <adityag2511@gmail.com>
2023-09-28 14:26:26 -0700

    Added code to send the location to a webserver

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { PermissionsAndroid, Platform, Text } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import axios from 'axios';
 
 const App = () => {
   const [latitude, setLatitude] = useState(null);
@@ -30,11 +31,24 @@ const App = () => {
     }
   };
 
+  const sendLocationToServer = async () => {
+    try {
+      const response = await axios.post('https://webhook.site/90efd798-cb95-4dce-953d-a98467c5f769', {
+        latitude,
+        longitude,
+      });
+      console.log('Location sent to server:', response.data);
+    } catch (error) {
+      console.log('Error sending location to server:', error);
+    }
+  };
+
   const getLocation = () => {
     Geolocation.getCurrentPosition(
       (position) => {
         setLatitude(position.coords.latitude);
         setLongitude(position.coords.longitude);
+        sendLocationToServer();
       },
       (error) => console.log(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
