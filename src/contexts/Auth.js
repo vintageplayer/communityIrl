aa81cca81606bfb146e4d6d31a6121efaa5ea972
Aditya Agarwal <adityag2511@gmail.com>
2023-10-14 16:09:21 -0700

    loading auth token from storage to check for existing user session when the auth context is initialized

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -1,23 +1,45 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AuthContext = createContext();
+const auth_data_key = '@auth_data'
 
 const AuthProvider = ({children}) => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [loading, setLoading] = useState(false);
+  //the AuthContext start with loading equals true
+  //and stay like this, until the data be load from Async Storage
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+  	//Every time the App is opened, this provider is rendered
+    //and loadAuthDataFromStorage() is invoked
+  	loadAuthDataFromStorage();
+  },[]);
+
+  async function loadAuthDataFromStorage() {
+  	try {
+  		const authDataSerialized = await AsyncStorage.getItem(auth_data_key);
+  		if (authDataSerialized) {
+  			const auth_token = JSON.parse(authDataSerialized);
+  			setIsLoggedIn(true);
+  		}
+  	} catch (error) {  		
+  	} finally {
+  		setLoading(false);
+  	}
+  }
 
 	const login = () => {
 		setLoading(true);
-		AsyncStorage.setItem('@auth_data', JSON.stringify('DUMMY_TOKEN'));
+		AsyncStorage.setItem(auth_data_key, JSON.stringify('DUMMY_TOKEN'));
 		setIsLoggedIn(true);
 		setLoading(false);
 	}
 
 	const logout = () => {
 		setIsLoggedIn(false);
-		AsyncStorage.removeItem('@auth_data');
+		AsyncStorage.removeItem(auth_data_key);
 	}
 
 	return (
